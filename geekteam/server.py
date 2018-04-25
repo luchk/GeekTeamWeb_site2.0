c8cfd4d5e66bcd7d11a0bd7ca078992771503ea8
luchk <30504944+luchk@users.noreply.github.com>
2018-04-25 14:09:38 +0300

    update0.1

--- geekteam/server.py
+++ geekteam/server.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename('uploads')
+#UPLOAD_FOLDER = '/Users/Vika/WebstormProjects/website/geekteam/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
@@ -17,15 +18,13 @@ def result():
     final_text = request.form["letter"]
     return render_template("index.html", final_text=final_text)
 
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
 #    file = request.files['image']
  #   f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
-    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
   #  file.save(f)
-    return render_template("index.html", final_text = "hjkkkkkkk")
+    return render_template("index.html", final_text = "test")
 
 
 if __name__ == "__main__":
